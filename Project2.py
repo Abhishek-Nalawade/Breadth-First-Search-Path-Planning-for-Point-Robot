074192037151f28e08e57b9dff692cce598b4f6b
AbhishekNarendraNalawade <abhi1793@umd.edu>
2021-03-05 19:42:16 -0500

    edited the remove from queue,  check_if_in_obstacle, check_if_visited_or_in_queue functions, defined the compare_with_goal function and added the main body of the code to call the functions

--- Project2.py
+++ Project2.py
@@ -19,6 +19,7 @@ class queue():
         if self.pending:
             return self.pending[-1]
 
+
     def size(self):
         return len(self.pending)
 
@@ -58,6 +59,8 @@ def obstacles(st):
 #removes from the queue
 def removing_from_queue():
     check = queue1.remove()
+    visited_list.append(check)
+    print("queue size ",queue1.size())
     return check
 
 
@@ -124,7 +127,7 @@ def super_move_function(currentnode):
     new_child.append(down_left(node))
     new_child.append(up_right(node))
     new_child.append(down_right(node))
-
+    #print(new_child)
     return new_child, node
 
 #checking if the node is in obstacle space
@@ -140,19 +143,58 @@ def check_if_in_obstacle_space(children, parent):
 
 ###############################
 #####check if the new node is there in the queue
-#checking if the node has been visited previously and then appending to the visited_list
-def check_if_visited(check):
-    for i in range(len(visited_list)):
+
+#checking if the node is in the queue or has been visited previously and then appending te parent to the visited_list
+def check_if_visited_or_in_queue(valid_children, parent):
+    note = list()
+    #new_valid_children = valid_children.copy()
+    for i in valid_children:
+        for j in range(queue1.size()):
+            if i == queue1.pending[j].current:
+                note.append(i)
+                #new_valid_children.pop(i)
+                break
+    new_valid_children = list()
+    for i in valid_children:
+        if i not in note:
+            new_valid_children.append(i)
+
+
+    note = list()
+    #ultimate_children = new_valid_children.copy()
+    for i in new_valid_children:
+        for j in visited_list:
+            if i == j.current:
+                note.append(i)
+                #ultimate_children.pop(i)
+                break
+    ultimate_children = list()
+    for i in new_valid_children:
+        if i not in note:
+            ultimate_children.append(i)
+
+
+
+
+
+    '''for i in range(len(visited_list)):
         if check.current == visited_list[i].current:
             #print("visited")
             return None
     visited_list.append(check)
-    #print("not visited")
-    return check
+    #print("not visited")'''
+    return ultimate_children, parent
 
 
 #compares new children with goal state and adds them to the queue
-#def compare_with_goal(children, parent):
+def compare_with_goal(ultimate_children, parent):
+    for child in ultimate_children:
+            if child == goal:
+                print("\n Goal has been reached \n")
+                return child, parent
+            else:
+                queue1.add(node(child, parent))
+                return None
 
 
 
@@ -161,6 +203,7 @@ def check_if_visited(check):
 canvas_size = [100,200]
 canvas = np.ones((canvas_size[0],canvas_size[1]))
 visited_list = []
+
 n = 1
 while n > 0:           #if the coordinate is in obstacle space display a message and ask again for coordinates
     start = list()
@@ -185,7 +228,20 @@ while n > 0:           #if the coordinate is in obstacle space display a message
         n = 0
 
 first_node = node(start, None)
-
+queue1 = queue()
+queue1.add(first_node)
+print(queue1.pending[0].current)
 print(start,goal)
+print(first_node.current)
+#print("it is this ",queue1.size())
 cv2.imshow("canvas",canvas)    #dimensions of the canvas
 cv2.waitKey(0)
+
+while True:
+    new_node = removing_from_queue()
+    children_list, parent = super_move_function(new_node)
+    valid_child, parent1 = check_if_in_obstacle_space(children_list, parent)
+    ultimate_child, parent2 = check_if_visited_or_in_queue(valid_child, parent1)
+    child_parent = compare_with_goal(ultimate_child, parent2)
+    if child_parent is not None:
+        break
